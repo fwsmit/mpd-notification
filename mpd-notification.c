553ab812900e7d8f01276c73aac071dbeef71d88
fwsmit <fw.smit01@gmail.com>
2022-02-12 08:38:08 +0100

    Remove workaround for libnotify bug
    This seems to not be a problem anymore. Notifications are showing up
    without needing the workaround.

--- mpd-notification.c
+++ mpd-notification.c
@@ -237,7 +237,7 @@ int main(int argc, char ** argv) {
 	char * notifystr = NULL;
 	GdkPixbuf * pixbuf = NULL;
 	GError * error = NULL;
-	enum mpd_state state = MPD_STATE_UNKNOWN, last_state = MPD_STATE_UNKNOWN;
+	enum mpd_state state = MPD_STATE_UNKNOWN;
 	const char * mpd_host, * mpd_port_str, * music_dir, * uri = NULL;
 	unsigned mpd_port = MPD_PORT, mpd_timeout = MPD_TIMEOUT, notification_timeout = NOTIFICATION_TIMEOUT;
 	struct mpd_song * song = NULL;
@@ -413,14 +413,6 @@ int main(int argc, char ** argv) {
 
 		state = mpd_status_get_state(mpd_recv_status(conn));
 		if (state == MPD_STATE_PLAY || state == MPD_STATE_PAUSE) {
-			/* There's a bug in libnotify where the server spec version is fetched
-			 * too late, which results in issue with image date. Make sure to
-			 * show a notification without image data (just generic icon) first. */
-			if (last_state != MPD_STATE_PLAY && last_state != MPD_STATE_PAUSE) {
-				notify_notification_update(notification, TEXT_TOPIC, "Starting playback...", ICON_AUDIO_X_GENERIC);
-				notify_notification_show(notification, NULL);
-			}
-
 			mpd_response_next(conn);
 
 			song = mpd_recv_song(conn);
@@ -479,8 +471,6 @@ int main(int argc, char ** argv) {
 #endif
 		} else
 			notifystr = strdup(TEXT_UNKNOWN);
-
-		last_state = state;
 
 		if (verbose > 0)
 			printf("%s: %s\n", program, notifystr);
